89c5b9a9d549079cf5b7abaf767ebcd1b51778ea
bigant1201 <gozygeory@gmail.com>
2025-10-16 16:59:20 -0700

    config commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Myportfolio/', // 👈 this MUST match your repo name
   server: {
     host: "::",
     port: 8080,
